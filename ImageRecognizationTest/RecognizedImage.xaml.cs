f024374169a7602abe2df6005e4ec6c5219078b9
Hirotada Watanabe <beqpad@hotmail.com>
2013-10-11 22:19:33 +0900

    コミットし忘れ。　ウィンドウ側　製造完了。

---
+++ ImageRecognizationTest/RecognizedImage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageRecognizationTest
+{
+    /// <summary>
+    /// RecognizedImage.xaml の相互作用ロジック
+    /// </summary>
+    public partial class RecognizedImage : UserControl
+    {
+        public RecognizedImage()
+        {
+            InitializeComponent();
+        }
+
+        private string _uriSource = null;
+
+        public string ImagePath
+        {
+            get { return _uriSource; }
+            set
+            {
+                if (value == null) return;
+
+                try
+                {
+                    var image = new BitmapImage();
+                    image.BeginInit();
+                    image.UriSource = new Uri(value);
+                    image.EndInit();
+
+                    _image.Source = image;
+                    _uriSource = value;
+                }
+                catch (NotSupportedException)
+                {
+                }
+
+            }
+        }
+
+        public string Text
+        {
+            get { return (string)_label.Content; }
+            set { _label.Content = value; }
+        }
+    }
+}
